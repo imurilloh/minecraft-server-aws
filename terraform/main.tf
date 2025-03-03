a0acaed4ec4ce9da9b31dbb74696ae65ddd2aa98
imurilloh <imurilloh@gmail.com>
2025-03-03 15:45:14 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,61 @@
 terraform {
   backend "s3" {
-    bucket = "devcraft-terraform-state" # Nombre de tu bucket
-    key    = "minecraft/terraform.tfstate" # Ruta dentro del bucket
-    region = "us-east-1" # Región de tu bucket
+    bucket = "devcraft-terraform-state"
+    key    = "minecraft/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_vpc" "minecraft_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "minecraft_vpc"
     Project = "DevCraft"
   }
 }
 
+resource "aws_internet_gateway" "minecraft_igw" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  tags = {
+    Name = "minecraft_igw"
+  }
+}
+
+resource "aws_route_table" "minecraft_rt" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.minecraft_igw.id
+  }
+
+  tags = {
+    Name = "minecraft_rt"
+  }
+}
+
 resource "aws_subnet" "minecraft_subnet" {
   vpc_id     = aws_vpc.minecraft_vpc.id
   cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true  # Habilita la asignación de IPs públicas
+  map_public_ip_on_launch = true
+
   tags = {
     Name = "minecraft_subnet"
     Project = "DevCraft"
   }
+}
+
+resource "aws_route_table_association" "minecraft_rta" {
+  subnet_id      = aws_subnet.minecraft_subnet.id
+  route_table_id = aws_route_table.minecraft_rt.id
 }
 
 resource "aws_security_group" "minecraft_sg" {
@@ -62,20 +93,20 @@ resource "aws_instance" "minecraft_server" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.minecraft_subnet.id
   vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
-  associate_public_ip_address = true  # Asignar IP pública
+  associate_public_ip_address = true
   key_name      = "devcraft"
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              apt-get update
-              apt-get install -y docker-ce
-              usermod -aG docker ubuntu
-              docker pull imurilloh/minecraft-server:latest
-              docker run -d -p 25565:25565 --name minecraft_server imurilloh/minecraft-server:latest
+                #!/bin/bash
+                apt-get update
+                apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+                add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                apt-get update
+                apt-get install -y docker-ce
+                usermod -aG docker ubuntu
+                docker pull imurilloh/minecraft-server:latest
+                docker run -d -p 25565:25565 --name minecraft_server imurilloh/minecraft-server:latest
               EOF
 
   tags = {
