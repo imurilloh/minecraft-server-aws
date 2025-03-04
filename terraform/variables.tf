e2e79469f87b40833881d4bbb505188bfe532070
Israel Murillo <israel.murillo.-nd@disney.com>
2025-03-03 19:09:45 -0600

    cambiando a mexico

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,18 @@
 variable "region" {
   description = "AWS region to deploy resources"
-  default     = "us-east-1"
+  default     = "mx-central-1"
+  #mx-central-1
+  #us-east-1
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.micro"
+  default     = "t3.medium"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-04b4f1a9cf54c11d0"
+  default     = "ami-04916108b2d40a326"
+  #ami-04916108b2d40a326 Mexico
+  #ami-04b4f1a9cf54c11d0 Virginia
 }
